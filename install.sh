b08b33d01649db05a5dccf98e15f86de150e601c
Dylan M. Taylor <dylan@dylanmtaylor.com>
2018-04-04 12:42:50 -0400

    Version 1 of the install.sh rewrite

--- install.sh
+++ install.sh
@@ -1,41 +1,32 @@
 #!/bin/bash
 
-# OS detect 
+# OS detection
 osdetect=$(uname)
 file_path="unknown"
-if [[ "$osdetect" == 'Linux' ]]; then
-    file_path="sudo mv fonts/* /usr/local/share/fonts/"
-elif [[ "$osdetect" == 'Darwin' ]]; then
-    file_path="mv fonts/* /Library/Fonts/"
-elif [[ "$osdetect" == 'Arch Linux' ]]; then
-    file_path="sudo mv fonts/* /usr/share/fonts/"
+if [ "$osdetect" == 'Darwin' ]]; then
+  file_path="/Library/Fonts/"
+elif [[ "$osdetect" == 'Linux' ]]; then
+  if [[ -d /usr/local/share/fonts/ ]]; then # Debian/Ubuntu and others.
+    file_path="/usr/local/share/fonts/"
+  elif [[ -d /usr/share/fonts/ ]]; then # OpenSUSE, Arch and other distros using this directory structure
+    mkdir -p /usr/share/fonts/google/
+    file_path="/usr/local/share/fonts/google/"
+  else # Fallback to installing fonts locally to the user, this is a safe bet, as several distros use this location.
+    mkdir -p ~/.fonts
+    file_path="~/.fonts"
+  fi
 fi
 
 clear
 echo "Installing all Google Web Fonts onto your System"
 echo "Downloading the fonts..."
-cd ~/Documents/
-curl -L https://github.com/google/fonts/tarball/master -o master.tar.gz
+curl -L https://github.com/google/fonts/tarball/master -o ~/Documents/master.tar.gz
 echo "Extracting the fonts..."
-mkdir -p goog-fonts/fonts
-tar -zxf master.tar.gz -C goog-fonts/fonts
-cd goog-fonts
-cd fonts
-find . -mindepth 2 -type f -print -exec mv {} . \;
-rm -R -- */
-rm *.txt
-rm *.json
-rm *.csv
-rm *.md
-rm *.html
-rm *.py
-rm AUTHORS
-rm CONTRIBUTORS
-cd ..
-$file_path
+mkdir -p ~/Documents/goog-fonts/fonts
+tar -zxf ~/Documents/master.tar.gz -C ~/Documents/goog-fonts/fonts
+sudo find goog-fonts/fonts/ -type f -name "*.ttf" -exec cp {} $file_path \;
 
 echo "Fonts installed; Cleaning up files..."
-cd ~/Documents/
-rm -f master.tar.gz
-rm -rf goog-fonts
+rm -f ~/Documents/master.tar.gz
+rm -rf ~/Documents/goog-fonts
 echo "All done! All Google Fonts installed."
